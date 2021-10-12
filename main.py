d497fc2e865601f2eef6b021d5b19f3a8f3afaf2
ChekeredList71 <naszpeti@gmail.com>
2021-10-12 18:03:13 +0200

    megjegyzések szerkesztve

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ from pybricks.tools import wait
 from pybricks.ev3devices import Motor
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3data = EV3Brick()
 sensordata = ColorSensor(Port.1)
 motordata = Motor(Port.A)
 
+#Program része
 motordata.run_forever()
 
 while True:
